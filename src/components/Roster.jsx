14f79642f93807eef2d67fe3e4e13bb96dc32c5b
Lexi Dorrity <lexidorrity@Lexis-MBP.lan>
2022-05-13 12:43:28 -0700

    so much work

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Piano from '../assets/img/thompiano.mp4'
 
 const Roster = () => {
   return (
-    <div>Roster</div>
+    <div>
+    <video autoPlay muted loop id="myVideo">
+  <source src={Piano} type="video/mp4"/>
+  Your browser does not support HTML5 video.
+</video>
+    <div className="rosterTitle">Roster
+    <div className="rosterNames">
+        <ul>
+            <li><a href='https://www.instagram.com/joshysoul/'target='_blank'>Joshy Soul</a></li>
+            <li><a href='https://www.instagram.com/vhscollection/'>VHS Collection</a></li>
+            <li><a href='https://www.instagram.com/thom.ko_/'>thom-ko</a></li>
+            <li><a href='https://www.instagram.com/moderneyesofficial/'>Modern Eyes</a></li>
+        </ul>
+        </div>
+        </div>
+        </div>
   )
 }
 
