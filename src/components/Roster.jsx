46107607a12aa832d319f0f79e8e90406c1ff518
Lexi Dorrity <lexidorrity@Lexis-MacBook-Pro.local>
2022-05-16 11:03:17 -0700

    changed video

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import Piano from '../assets/img/thompiano.mp4'
+import React from "react";
+import Piano from "../assets/img/thompianotwo.mp4";
 
 const Roster = () => {
   return (
     <div>
-    <video autoPlay muted loop id="myVideo">
-  <source src={Piano} type="video/mp4"/>
-  Your browser does not support HTML5 video.
-</video>
-    <div className="rosterTitle">Roster
-    <div className="rosterNames">
-        <ul>
-            <li><a href='https://www.instagram.com/joshysoul/'target='_blank'>Joshy Soul</a></li>
-            <li><a href='https://www.instagram.com/vhscollection/'>VHS Collection</a></li>
-            <li><a href='https://www.instagram.com/thom.ko_/'>thom-ko</a></li>
-            <li><a href='https://www.instagram.com/moderneyesofficial/'>Modern Eyes</a></li>
-        </ul>
+      <video autoPlay muted loop id="myVideo">
+        <source src={Piano} type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+      <div className="rosterTitle">
+        Roster
+        <div className="rosterNames">
+          <ul>
+            <li>
+              <a href="https://www.instagram.com/joshysoul/" target="_blank">
+                Joshy Soul
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/vhscollection/"
+                target="_blank"
+              >
+                VHS Collection
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/thom.ko_/" target="_blank">
+                thom-ko
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/moderneyesofficial/"
+                target="_blank"
+              >
+                Modern Eyes
+              </a>
+            </li>
+          </ul>
         </div>
-        </div>
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Roster
+export default Roster;
