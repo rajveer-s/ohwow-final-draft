5bfccd1dd046c77fb1305c610de483e2e5b6d0ae
Lexi Dorrity <lexidorrity@Lexis-MBP.lan>
2022-05-18 12:24:39 -0700

    thom page

--- src/components/Thomko.jsx
+++ src/components/Thomko.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
-import './Profile.css';
+import "./Profile.css";
 import backbutton from '../assets/img/backbuttoncolor.png'
 import { Link } from 'react-router-dom';
-import thomprofile from '../assets/img/thomprofile.TIF'
+import thomprofile from '../assets/img/thomprofile2.jpg'
 
-
-const style = {
-    height: "100vh",
-    background: "white",
-    color: "black"
-}
 
 const Thomko = () => {
   return (
-    <div style={style}> 
+    <div className="profileContainer"> 
         <div className="backbutton">
             <Link to="/roster"><img src={backbutton} alt="back button img" /></Link>
-        <div className="artistImage">
-            <img src={thomprofile} alt="artist image" />
         </div>
+        <div className="artistDiv">
             
+            <img className= "artistImage" src={thomprofile} alt="artist image" />
+            <div className="artistInfo">
+            <h1>THOM.KO</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim consequuntur expedita sit, aliquam delectus tenetur asperiores alias officia voluptatem magni labore ad aliquid repudiandae repellendus debitis, eligendi praesentium maiores velit?</p>
+        </div> 
 
         </div>
     </div>
