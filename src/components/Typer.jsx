c69d6edc1eae1518f686ecbb149fa731cac259e4
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-18 19:09:26 -0700

    loop page turned to 404

--- src/components/Typer.jsx
+++ src/components/Typer.jsx
@@ -4,18 +4,15 @@ import { useTypewriter, Cursor} from 'react-simple-typewriter'
 const Typerwriter = () => {
 
   const {text} = useTypewriter({
-    words: ['Developer', 'Programmer', 'stylist', 'Genuis'],
-    loop:{},
+    words: ['Uh oh ... 404 Page not found' ],
     delaySpeed: 2000,
     deleteSpeed: 90,
     typeSpeed: 100
   })
 
   return (
-    <div>
-        <h1 style={{ paddingTop: '15rem', margin: 'auto 0', fontWeight: 'bold', fontSize: '8rem' }}>I am a </h1>
-      <span style={{ color: 'red', fontWeight: 'bold', fontSize: '10rem' }}>{text}</span>
-      <Cursor  />
+    <div className='loopdiv' style={{background: 'black', height: '100vh'}}>
+      <span style={{ color: 'white', fontSize: '3rem' }}>{text}</span>
     </div>
   )
 }
