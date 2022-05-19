d724630b3fdc78e9799155c28c241ed66e99996f
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-05-19 10:06:50 -0700

    added Joshy page

--- src/components/JoshySoul.jsx
+++ src/components/JoshySoul.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
-import curvyimg from '../assets/img/homeimg.png'
-import './josh.css';
-import a from '../assets/img/a.png'
-import backbutton from '../assets/img/backbuttoncolor.png'
+import "./Profile.css";
+import backbutton from '../assets/img/backbutton4.png'
 import { Link } from 'react-router-dom';
+import thomprofile from '../assets/img/thomprofile2.jpg'
+import { FaInstagram, FaFacebookSquare, FaSpotify  } from 'react-icons/fa'
 
 
-const style = {
-    height: "100vh",
-    background: "white",
-    color: "black"
-}
-
-const JoshySoul = () => {
+const Joshysoul = () => {
   return (
-    <div style={style}> 
+    <div className="profileContainer"> 
         <div className="backbutton">
             <Link to="/roster"><img src={backbutton} alt="back button img" /></Link>
+        </div>
+        <div className="artistDiv fade-in-text">
             
+            <img className= "artistImage" src={thomprofile} alt="artist image" />
+            <div className="artistInfo">
+            <h1>THOM.KO</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim consequuntur expedita sit, aliquam delectus tenetur asperiores alias officia voluptatem magni labore ad aliquid repudiandae repellendus debitis, eligendi praesentium maiores velit?</p>
+              <a href="https://www.instagram.com/thom.ko_/" target="_blank"><FaInstagram color='black' size='2em' style={{margin: '2rem'}}/></a>
+              <a href="https://www.facebook.com" target="_blank"><FaFacebookSquare color='black' size='2em' style={{margin: '2rem'}} /></a> 
+              <a href="https://open.spotify.com/artist/3dWWHuyPD2YOd7B8geKidf" target="_blank"><FaSpotify color='black' size='2em' style={{margin: '2rem'}} /></a>
+        </div> 
 
         </div>
     </div>
   )
 }
 
-export default JoshySoul;
+export default Joshysoul;
--- src/components/Thomko.jsx
+++ src/components/Thomko.jsx
@@ -3,7 +3,7 @@ import "./Profile.css";
 import backbutton from '../assets/img/backbutton4.png'
 import { Link } from 'react-router-dom';
 import thomprofile from '../assets/img/thomprofile2.jpg'
-import { FaInstagram, FaFacebookSquare, FaLinkedinIn  } from 'react-icons/fa'
+import { FaInstagram, FaFacebookSquare, FaSpotify  } from 'react-icons/fa'
 
 
 const Thomko = () => {
@@ -18,9 +18,9 @@ const Thomko = () => {
             <div className="artistInfo">
             <h1>THOM.KO</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim consequuntur expedita sit, aliquam delectus tenetur asperiores alias officia voluptatem magni labore ad aliquid repudiandae repellendus debitis, eligendi praesentium maiores velit?</p>
-              <a href="https://www.instagram.com" target="_blank"><FaInstagram color='black' size='2em' style={{margin: '2rem'}}/></a>
+              <a href="https://www.instagram.com/thom.ko_/" target="_blank"><FaInstagram color='black' size='2em' style={{margin: '2rem'}}/></a>
               <a href="https://www.facebook.com" target="_blank"><FaFacebookSquare color='black' size='2em' style={{margin: '2rem'}} /></a> 
-              <a href="https://www.linkedin.com" target="_blank"><FaLinkedinIn color='black' size='2em' style={{margin: '2rem'}} /></a>
+              <a href="https://open.spotify.com/artist/3dWWHuyPD2YOd7B8geKidf" target="_blank"><FaSpotify color='black' size='2em' style={{margin: '2rem'}} /></a>
         </div> 
 
         </div>
